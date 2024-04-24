487672e0f91f501ef679e1900db1fc730def04e7
Maksym <m.bovsunivskyi@expoplatform.com>
2024-04-24 14:51:22 +0300

    add new scenario

---
+++ tests4/test24.spec.ts
@@ -0,0 +1,15 @@
+import {test} from "@playwright/test";
+import {faker} from "@faker-js/faker";
+import arr  from "../arr.json";
+const link = 'https://pharmaevents.proximaresearch.com/en/event-216/tr?token='
+
+test('play video 1', async ({page}) => {
+    const currentDateHash = arr[166].invited_link_token;
+    const userName = faker.internet.userName();
+    await page.goto(`${link}${currentDateHash}`);
+    await page.locator('#name').fill(userName);
+    await page.locator('.btn').click();
+    await new Promise(resolve => setTimeout(resolve, 10000));
+    await new Promise(resolve => setTimeout(resolve, 600000));
+    console.log('wait 60 sec');
+})
